580d4639fd7ce271780b2baab271c0abb2968d65
A-F-G-A <aguzmana@unicartagena.edu.co>
2025-09-19 19:08:01 -0500

    Se eliminan los talleres para guardarlos en el proyecto

--- Taller_9/Ejercicio_2/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Pez pez = new Pez("Tilapia", "Agua dulce");
-        pez.MostrarEspecie();
-    }
-}
--- Taller_9/Ejercicio_3/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // Esto genera error de compilación porque super solo
-        /// puede usarse dentro de una clase derivada.
-        super.toString(); 
-    }
-}
