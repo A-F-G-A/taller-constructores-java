bf8f35191c2970f9c2001845b268035a0f38a845
A-F-G-A <aguzmana@unicartagena.edu.co>
2025-09-19 19:07:54 -0500

    Se eliminan los talleres para guardarlos en el proyecto

--- Taller_8/Ejercicio_1/PruebaCoche.java
+++
@@ -1,6 +0,0 @@
-public class PruebaCoche {
-    public static void main(String[] args) {
-        Coche coche = new Coche("Toyota", 220, 4);
-        coche.MostrarInfo();
-    }
-}
--- Taller_8/Ejercicio_2/PruebaEstudiante.java
+++
@@ -1,7 +0,0 @@
-
-public class PruebaEstudiante {
-    public static void main(String[] args) {
-        Estudiante est = new Estudiante("Andrés", 20, "2025A123");
-        est.MostrarDetalles();
-    }
-}
